f092f0ec7899706c232f3186f6637a2bc989ec5f
Jeroen B <jeroenb04@live.com>
2024-10-02 13:06:59 +0200

    Initial Class setup
    Co-authored-by: jeske5 <jeske5@users.noreply.github.com>

--- 2_buffer/main.cpp
+++ 2_buffer/main.cpp
@@ -22,6 +22,50 @@
 // although it is good habit, you don't have to type 'std::' before many objects by including this line
 using namespace std;
 
+// TD: Case logic append / remove
+// TD: Logging
+// TD: Logger write -> Case distinctions
+// TD: Parallelism
+
+class Buffer {
+public:
+  Buffer(bool bounded, int bound = 0)
+    : bounded(bounded), bound_limit(bound) {}
+
+  void append(int i) {
+    b.push_back(i);
+  }
+  int remove() {
+    int r = b.front();
+    b.erase(b.begin());
+    return r;
+  }
+  void bound(int b) {
+    bounded = true;
+    bound_limit = b;
+  }
+  void unbound() {
+    bounded = false;
+    bound_limit = 0;
+  }
+
+private:
+  vector<int> b;
+  bool bounded;
+  int bound_limit;
+};
+
+class Logger {
+public:
+  void write(string op, bool did_work) {}
+
+  string read(int idx) {
+    return l.at(idx);
+  }
+private:
+  vector<string> l;
+};
+
 
 int main(int argc, char* argv[]) {
 	return 0;
